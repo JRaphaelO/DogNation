c82cb52392a001cd9c0d1a5ec8f1c4d93353a23e
renan-goes <36930305+Renan-Goes@users.noreply.github.com>
2020-09-11 22:11:11 -0300

    add interactive leafletjs map

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,33 +1,19 @@
-
-$('[name:"HeaderButton"]').click(function() {
-    $('html, body').animate({
-        scrollTop: $('[name:"Header"]').offset().top
-    }, 2000);
-});
-
-$('[name:"IamButton"]').click(function() {
-    $('html, body').animate({
-        scrollTop: $('[name:"Iam"]').offset().top
-    }, 2000);
-});
-
-$('[name:"DogsButton"]').click(function() {
-    $('html, body').animate({
-        scrollTop: $('[name:"Dogs"]').offset().top
-    }, 2000);
-});
-
-$('[name="ContactsButton"]').click(function() {
-        $('html, body').animate({
-            scrollTop: $('[name="teste"]').offset().top
-        }, 2000);
-    });
-
-$('button').click(function() {
-    var thisButton = $(this).name;
+$('#MenuBar .nav-item .nav-link').click(function(event) {
+    event.preventDefault();
+    var thisButton = $(this).attr('name');
     var section = thisButton.substring(0, thisButton.length - 6);
-
+   // alert($('#' + section).offset().top);
+   
     $('html, body').animate({
         scrollTop: $('#' + section).offset().top
     }, 2000);
 });
+
+// $('button').click(function() {
+//     var thisButton = $(this).name;
+//     var section = thisButton.substring(0, thisButton.length - 6);
+
+//     $('html, body').animate({
+//         scrollTop: $('#' + section).offset().top
+//     }, 2000);
+// });
