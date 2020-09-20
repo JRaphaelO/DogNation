d9d7e64031a611b165c9fcd8198c32a30782e241
Julio Raphael <jraphaeloliveira14@gmail.com>
2020-09-19 23:44:04 -0300

    Ajustes nos textos e botões

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+$(window).scroll(function() {
+    var scrollPosBottom = $(document).scrollTop() + $(window).height();
+    var scrollPos = $(document).scrollTop();
+    var mapScrollPos = $('#mapid').offset().top + $('#mapid').height();
+    var mapScrollPosTop = $('#mapid').offset().top;
+    console.log('Map: ' + scrollPos, scrollPosBottom, mapScrollPos, mapScrollPosTop);
+
+    if (scrollPos > mapScrollPos) {
+        $('#mapid').stop().fadeTo(10, 0);
+    }
+    else if (scrollPosBottom < mapScrollPosTop) {
+        $('#mapid').stop().fadeTo(10, 0);
+    }
+    else {
+        $('#mapid').stop().fadeTo(500, 1);
+    }
+});
+
+$(window).scroll(function() {
+    var scrollPosBottom = $(document).scrollTop() + $(window).height();
+    var scrollPos = $(document).scrollTop();
+    var imgsScrollPos = $('.header-imgs').offset().top + $('.header-imgs').height();
+    var imgsScrollPosTop = $('.header-imgs').offset().top;
+    console.log('Imgs: ' + scrollPos, scrollPosBottom, imgsScrollPos, imgsScrollPosTop);
+
+
+    if (scrollPos > imgsScrollPos) {
+        $('.header-imgs').stop().fadeTo(10, 0);
+    }
+    else {
+        $('.header-imgs').stop().fadeTo(500, 1);
+    }
+});
